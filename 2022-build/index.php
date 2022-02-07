9ebdf35684efda8d9f929ae72ce02e3d5b9876f1
blackaeonium <83319369+blackaeonium@users.noreply.github.com>
2022-02-07 17:43:09 +1100

    updated bootstrap version to 5.1.3

--- 2022-build/index.php
+++ 2022-build/index.php
@@ -9,8 +9,7 @@
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="../2022-build/assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../2022-build/css/styles.css" rel="stylesheet" />
-			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
