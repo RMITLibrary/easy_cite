a43ea8821a8729a006dfdff94fc84d7366d0e293
Karl Ervine <karl.ervine@rmit.edu.au>
2025-02-17 09:50:44 +1100

    feat: update transcript

--- includes/content_home_intro.php
+++ includes/content_home_intro.php
@@ -24,89 +24,24 @@
                 <div id="Transcript-collapseTwo" class="accordion-collapse collapse"
                     aria-labelledby="Transcript-headingTwo">
                     <div class="accordion-body">
-                        <p>
-                            Easy Cite is an online tool that provides examples and referencing tips for a
-                            wide range of styles used at RMIT.
-                        </p>
-                        <p>
-                            Easy Cite is intended as a guide only and some styles are open to
-                            interpretation. You should always check with your teacher or lecturer to ensure
-                            you are using the correct style for your assessment tasks. 
-                        </p>
-                        <p>
-                            The Easy Cite web page has four sections: 
-                        </p>
-                        <p>
-                            A navigation menu at the top of the screen links to the “How to use Easy Cite”
-                            pop-down area. Instructions for using the Easy Cite tool can be found here. You
-                            can open or close this area at any time. 
-                        </p>
-                        <p>
-                            A style guide tab menu sits under the navigation menu. This is where you select
-                            the correct style guide for your assignment or task. 
-                        </p>
-                        <p>
-                            A source type menu is on the left side of the screen. This is where you select
-                            the different types of sources that you need to reference. You may be
-                            referencing books, journal articles, websites, audiovisual media such as
-                            podcasts or YouTube, or many other types of sources. 
-                        </p>
-                        <p>
-                            The details of the referencing source sub-type sit in the main body of the
-                            screen. This provides detailed options about your source material. For example:
-                            it shows how to cite a book with multiple authors, or how to cite figures /
-                            photos or diagrams you might use in your assignment.
-                        </p>
-                        <p>
-                            Note that each referencing style outlines the general principles of how to use
-                            that style. This ‘Using’ tab explains important information about the style. It
-                            gives general rules for in-text and reference list citations, and an example of
-                            a reference list. Please read these rules carefully.
-                        </p>
-                        <p>
-                            Each sub-type category provides examples of both in-text and reference list
-                            citations.  
-                        </p>
-                        <p>
-                            When using information from a source text, you can express it as a direct quote
-                            or a paraphrase.
-                            When directly quoting, you must use the exact words of the author and put them
-                            in quotation marks.
-                            Be sure to keep direct quotes to a minimum.  
-                            The paraphrase in-text example will be suitable if you’re using your own words
-                            to express an author’s idea. Paraphrasing is the preferred method of citing
-                            information.
-                        </p>
-                        <p>
-                            If writing a reference list citation, you must have bibliographic details such
-                            as author names, year of publication, book title and publisher details.
-                        </p>
-                        <p>
-                            Each different referencing style requires different elements and formatting.
-                            Make sure to pay close attention to the order of these elements.
-                        </p>
-                        <p>
-                            Pay attention to the use of capitalisation, italics, commas, and colons, as well
-                            as formatting such as hanging indents. These are just as important for
-                            presenting the citation accurately.
-                        </p>
-                        <p>
-                            There are three options for printing all or any part of the Easy Cite style
-                            guide: print the whole guide for your referencing style; print only the source
-                            type that you need, or you can just print the sub-type of your source type
-                            material.
-                        </p>
-                        <p>
-                            You can send your selection to a printer for a hard copy or choose to save it as
-                            a PDF file for off-line access.
-                        </p>
-                        <p>
-                            You can also send feedback about Easy Cite via the link at the bottom of the
-                            page.
-                        </p>
-                        <p>
-                            If you need more help, please visit Ask the Library or the Referencing section
-                            of the Learning Lab. </p>
+                        <h2>How to use the Easy Cite referencing tool</h2>
+                        <p>Easy Cite is an online tool that provides examples and tips for a wide range of referencing styles used at RMIT.</p>
+                        <p>Easy Cite is only intended as a guide as some styles are open to interpretation. You should always check with your teacher or lecturer to make sure you are using the correct style for your assessment tasks. </p>
+                        <p>Easy Cite is keyboard and screen-reader accessible, and you can switch between light and dark mode with the buttons near the bottom of the page.</p>
+                        <p>The home page has instructions with a video and transcript. Select ‘Easy Cite’ at the top of the page to return here at any time.</p>
+                        <p>Select the correct style guide for your assignment or task from the links at the top of the page. </p>
+                        <p>Each referencing style begins with an introduction which outlines the general principles of how to use that style. It gives general rules for in-text and reference list citations, and an example of a reference list. Please read these rules carefully.</p>
+                        <p>Select the type of source that you need to reference from the right-hand menu, for example: books, journal articles, websites, or audiovisual media such as podcasts and videos. </p>
+                        <p>The body of the page provides details about the referencing source sub-type. Select the headings to open information about your specific type of source material, for example: how to cite a book with multiple authors, or how to cite tables and graphs.</p>
+                        <p>Each sub-type category provides examples of both in-text and reference list citations.  </p>
+                        <p>When using information from a source text, you can express it as a paraphrase or a direct quote.</p>
+                        <p>The paraphrasing in-text example is suitable if you are using your own words to express an author’s idea. Paraphrasing is the preferred method of citing information.</p>
+                        <p>When directly quoting, you must use the exact words of the author and put them in quotation marks. Keep direct quotes to a minimum.  </p>
+                        <p>If you are writing a reference list citation, you must include bibliographic details such as author, year of publication, book title and publisher. Each referencing style requires different elements and formatting. The correct order of these elements is important.</p>
+                        <p>Pay close attention to capitalisation, italics, commas and colons, as well as formatting such as hanging indents. These are essential for presenting the citation accurately.</p>
+                        <p>Buttons at the bottom of the text allow you to print the whole guide for your referencing style, or just the source type that you need. Send your selection to a printer or save it as a PDF for offline access.</p>
+                        <p>You can send feedback about Easy Cite via the link at the bottom of the page.</p>
+                        <p>If you need more help with referencing, you can open the ‘Ask the Library’ chat window in the bottom corner of the page or visit the Library’s Referencing help via the link at the top of the page. </p>
                     </div>
                 </div>
             </div>
